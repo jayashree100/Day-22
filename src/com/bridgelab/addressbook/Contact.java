cad29463e3ddb4e5cc894d76b5f3ae4ac3ada0c0
JAISHRI <sjayashree529@gmail.com>
2022-04-01 15:35:36 +0530

    UC-1

---
+++ src/com/bridgelab/addressbook/Contact.java
@@ -0,0 +1,12 @@
+package com.bridgelab.addressbook;
+
+public class Contact {
+	String firstName;
+	String lastName;
+	String address;
+	String city;
+	String state;
+	String zip;
+	String email;
+
+}
---
+++ src/com/bridgelab/addressbook/Main.java
@@ -0,0 +1,10 @@
+package com.bridgelab.addressbook;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Wel Come to AddressBook");
+
+	}
+
+}
