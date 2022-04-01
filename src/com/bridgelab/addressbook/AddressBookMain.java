b22fc94a5aa20bf625b9510fd68bc27dcdf50103
JAISHRI <sjayashree529@gmail.com>
2022-04-01 17:37:53 +0530

    UC-2

---
+++ src/com/bridgelab/addressbook/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelab.addressbook;
+
+public class AddressBookMain
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome To Address Book Program");
+        AddressBook addressBook=new AddressBook();
+        addressBook.operation();
+    }
+}
